a6ddac915d3c453e03f818e39d3ae1893a5be96d
Unknown <ozan.kalan1@gmail.com>
2017-10-02 20:25:18 -0500

    woops forgot one change

--- app/src/main/java/com/example/ozan_laptop/bakingapp/models/RecipeList.java
+++ app/src/main/java/com/example/ozan_laptop/bakingapp/models/RecipeList.java
@@ -1,8 +1,18 @@
 package com.example.ozan_laptop.bakingapp.models;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by ozan-laptop on 10/2/2017.
  */
 
 public class RecipeList {
+
+    @SerializedName("results")
+    @Expose
+    public List<Recipe> recipeResults = null;
+
 }
